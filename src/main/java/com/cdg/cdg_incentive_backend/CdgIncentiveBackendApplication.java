9c3a8f36d7a1d4291829e298a45cc57ed38cde52
Thanakrit <tanapol@Thanakrits-MacBook-Pro.local>
2024-08-12 17:41:38 +0700

    first commit

---
+++ src/main/java/com/cdg/cdg_incentive_backend/CdgIncentiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cdg.cdg_incentive_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdgIncentiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdgIncentiveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdg/cdg_incentive_backend/CdgIncentiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdg.cdg_incentive_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdgIncentiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
